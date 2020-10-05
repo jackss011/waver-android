28420670b412361fa1108bca85ff588ccea42a68
Giacomo Agnetti <jackss.code@gmail.com>
2020-10-05 22:06:13 +0200

    Created SoundService

---
+++ app/src/main/java/com/jack/nars/waver/SoundService.kt
@@ -0,0 +1,12 @@
+package com.jack.nars.waver
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class SoundService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
