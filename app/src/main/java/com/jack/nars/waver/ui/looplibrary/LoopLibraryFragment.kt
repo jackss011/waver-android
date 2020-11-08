27c6fe52eb7ca168b0104b9f9408b624db49b009
Giacomo Agnetti <jackss.code@gmail.com>
2020-11-08 01:22:59 +0100

    Added simple loop library

---
+++ app/src/main/java/com/jack/nars/waver/ui/looplibrary/LoopLibraryFragment.kt
@@ -0,0 +1,58 @@
+package com.jack.nars.waver.ui.looplibrary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.viewModels
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import com.jack.nars.waver.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_loop_library.*
+import timber.log.Timber
+
+
+@AndroidEntryPoint
+class LoopLibraryFragment : Fragment() {
+    private val viewModel: LoopLibraryModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_loop_library, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val chipGroup = getView()?.findViewById<ChipGroup>(R.id.chipgroup) ?: return
+
+        viewModel.staticLoops.forEach { loop ->
+            val chip = Chip(context).apply {
+                text = loop.title
+                id = ViewCompat.generateViewId()
+                tag = loop.id
+                isCheckable = true
+                setOnClickListener {
+                    Timber.d("Clicked: ${loop.id}, set to ${this.isChecked}}")
+                }
+            }
+
+            chipGroup.addView(chip)
+        }
+
+        viewModel.activeLoopIds.observe(requireActivity()) { activeIds ->
+            viewModel.staticLoops.forEach { loop ->
+                chipGroup.findViewWithTag<Chip>(loop.id)?.also { chip ->
+                    val active = loop.id in activeIds
+                    chip.isChecked = active
+                    Timber.d("Setting chip ${loop.id} to $active")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/jack/nars/waver/ui/looplibrary/LoopLibraryModel.kt
@@ -0,0 +1,17 @@
+package com.jack.nars.waver.ui.looplibrary
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import com.jack.nars.waver.data.LoopRepository
+
+
+class LoopLibraryModel @ViewModelInject
+constructor(private val loopRepository: LoopRepository) : ViewModel() {
+
+    val staticLoops = loopRepository.staticLoops
+    val activeLoopIds = loopRepository.activeCompositionData
+        .map { composition -> composition?.loops?.map { it.id } ?: emptyList() }
+
+    fun onLoopClicked() {}
+}
