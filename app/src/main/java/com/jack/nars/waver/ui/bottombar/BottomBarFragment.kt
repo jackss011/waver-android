5da437e99b107dcfd3073e0ba2f8cd8669ab574f
Giacomo Agnetti <jackss.code@gmail.com>
2020-11-12 03:36:09 +0100

    Improved bottom bar layout

--- app/src/main/java/com/jack/nars/waver/ui/bottombar/BottomBarFragment.kt
+++ app/src/main/java/com/jack/nars/waver/ui/bottombar/BottomBarFragment.kt
@@ -1,16 +1,22 @@
 package com.jack.nars.waver.ui.bottombar
 
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.InverseBindingAdapter
 import androidx.databinding.InverseBindingListener
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.slider.Slider
 import com.jack.nars.waver.MainModel
 import com.jack.nars.waver.R
@@ -48,5 +54,17 @@ class BottomBarFragment : Fragment() {
         }
 
         return binding.root
+    }
+}
+
+
+@BindingAdapter("playIcon")
+fun bindPlayIcon(btn: MaterialButton, isPlaying: Boolean?) {
+    isPlaying?.let {
+        btn.icon = ResourcesCompat.getDrawable(
+            btn.context.resources,
+            if (!isPlaying) R.drawable.ic_play_24 else R.drawable.ic_pause_24,
+            null
+        )
     }
 }
